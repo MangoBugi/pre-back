a82c9690ba509b5c9459006fd68fe32da506d917
221B0825 <eunseoyu0825@gmail.com>
2026-02-15 15:56:12 +0900

    feat: add auth check

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
@@ -15,9 +16,11 @@ const PORT = ENV.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server running on port : " + PORT)
